0b7cab458c62db0453cf6cd80f3aa0f497aeb078
Parker Berberian <pberberian@iol.unh.edu>
2017-12-20 13:26:23 -0500

    Adds update workflow
    JIRA: None
    
    Adds a workflow and code that installs the chosen operating system on
    a free host, updates it, and captures the new disk image.
    This ensures that when we provision a new host, it is relatively up to date.
    
    Change-Id: I553e4c8c1715add47ebfe7d853f884f242813ab2
    Signed-off-by: Parker Berberian <pberberian@iol.unh.edu>

---
+++ pharoslaas/actions/update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+##############################################################################
+# Copyright 2017 Parker Berberian and Others                                 #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License");            #
+# you may not use this file except in compliance with the License.           #
+# You may obtain a copy of the License at                                    #
+#                                                                            #
+#    http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+##############################################################################
+
+#check which package manager is used and run appropriate update cmd
+
+if which apt; then
+    apt -y update && apt -y upgrade
+elif which yum; then
+    yum -y upgrade
+elif which zypper; then
+        zypper -n update
+else
+    echo "Could not find package manager"
+    exit 1
+fi
